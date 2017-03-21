15e605dd4fac3041cd1b6ff3451aefe66c0b2370
William Barela <william.barela.dev@gmail.com>
2017-03-21 22:41:56 +0900

    updating

--- ruby/solution.rb
+++ ruby/solution.rb
@@ -1,7 +1,7 @@
 def animals(heads, legs)
     chickens = -0.5 * legs + 2 * heads
     cows = 0.5 * legs - heads
-
-    # checks if chickens or cows are non-negative integers.
+    
+	# checks if chickens or cows are non-negative integers.
     (chickens < 0 || cows < 0 || !(chickens % 1 == 0) || !(cows % 1 == 0))? "No solutions" : [chickens, cows]
 end
